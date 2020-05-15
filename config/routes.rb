0178bc90fec4723dc89964af389f81ece497dca2
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-05-15 18:40:20 +0100

    Create session for log in users and create form and style it for the purpose

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
 
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  get  'signup',  to: 'users#new'
+  post 'signup',  to: 'users#create'
+  get  'profile', to: 'users#show'
+
+  get    'login',  to: 'sessions#new'
+  post   'login',  to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 end

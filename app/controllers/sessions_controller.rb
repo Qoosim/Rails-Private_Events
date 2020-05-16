7598748c63569f7fca4d6bd71d5e238fe44f71df
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-05-16 11:50:21 +0100

    Design static home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new; end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
       flash.now[:notice] = 'You have been logged in!'
-      redirect_to @user
+      redirect_to user_path(user)
     else
+      flash.now[:notice] = 'Invalid email/password combination!'
       render 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
 
   get  'signup',  to: 'users#new'
   post 'signup',  to: 'users#create'
